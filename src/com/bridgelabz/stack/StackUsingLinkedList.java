7a66c9c53d78a7c957d9f98f5bddb6d85ac0c972
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-08 18:51:24 +0530

    Adde:code to create stack using linked list

--- src/com/bridgelabz/stack/StackUsingLinkedList.java
+++ src/com/bridgelabz/stack/StackUsingLinkedList.java
@@ -1,9 +1,58 @@
 package com.bridgelabz.stack;
+class Node<T> {
+    T data;
+    Node<T> next;
 
+    Node(T data) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class List<T> {
+    private int len = 0;
+    Node<T> top;
+
+    List() {
+        this.top = null;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        len++;
+    }
+    public String toString() {
+
+        String S = "{";
+
+        Node<T> X = top;
+
+        if (X == null) return S + "}";
+
+        while (X.next != null) {
+            S += String.valueOf(X.data) + "->";
+            X = X.next;
+        }
+
+        S += String.valueOf(X.data);
+        return S + "}";
+    }
+
+}
 public class StackUsingLinkedList {
     public static void main(String[] args) {
-        System.out.println("Welcome to Stack Program");
-        
+        List<Integer> stack = new List<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(50);
+        System.out.println("stack is "+stack);
+
     }
 
 }
