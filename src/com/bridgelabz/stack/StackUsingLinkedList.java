67cf14070a7587c21894776634c94cc2156a2bc0
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-08 18:47:33 +0530

    Added:Welcome Message to master

---
+++ src/com/bridgelabz/stack/StackUsingLinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stack;
+
+public class StackUsingLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack Program");
+        
+    }
+
+}
